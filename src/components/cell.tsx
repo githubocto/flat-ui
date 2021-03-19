751084b4586eb648f389f79d79e7ed242a6fbc2b
Amelia Wattenbeger <wattenberger@github.com>
2021-03-19 16:37:58 -0400

    fix: let user select long cell value

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -40,7 +40,7 @@ export const Cell = React.memo(function(props: CellProps) {
   const { cell: CellComponent } = cellInfo;
 
   const cellClass = cc([
-    'group flex flex-none items-center px-4 border-b border-r',
+    'cell group flex flex-none items-center px-4 border-b border-r',
     {
       'text-gray-300': typeof value === 'undefined',
       'border-green-200': status === 'new',
@@ -83,13 +83,14 @@ export const Cell = React.memo(function(props: CellProps) {
 
       {isLongValue && (
         <div
-          className={`absolute ${isNearBottomEdge ? 'bottom-0' : 'top-0'} ${
+          className={`cell__long-value absolute ${
+            isNearBottomEdge ? 'bottom-0' : 'top-0'
+          } ${
             isNearRightEdge ? 'right-0' : 'left-0'
           } p-4 py-2 bg-white opacity-0 group-hover:opacity-100 z-30 border border-gray-200 shadow-md pointer-events-none`}
           style={{
             width: 'max-content',
             maxWidth: '27em',
-            margin: -1,
           }}
         >
           <div className="line-clamp-9">{formattedValue || value}</div>
