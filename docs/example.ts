d6e168018e982fca47ec806fa2c1375eac3a799e
Amelia Wattenberger <wattenberger@github.com>
2022-10-31 19:40:38 -0700

    Update example.ts

--- docs/example.ts
+++ docs/example.ts
@@ -1,7 +1,12 @@
 import { Grid } from '@githubocto/flat-ui';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
 
 const MyComponent = () => {
   const data = [{ column1: 123 }, { column1: 234 }];
 
   return <Grid data={data} />;
 };
+
+const root = ReactDOM.createRoot(document.body)
+root.render(<MyComponent />)
