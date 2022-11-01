7b4d43e35ee2a0040d1b81dc3afd5fe427e3ff66
Amelia Wattenberger <wattenberger@github.com>
2022-10-31 19:50:08 -0700

    Update example.ts

--- docs/example.ts
+++ docs/example.ts
@@ -1,12 +1,17 @@
 import { Grid } from '@githubocto/flat-ui';
-import React from 'react';
-import * as ReactDOM from 'react-dom';
 
-const MyComponent = () => {
-  const data = [{ column1: 123 }, { column1: 234 }];
-
+const Spreadsheet = () => {
   return <Grid data={data} />;
 };
 
+const data = [
+  { column1: 123, color: "green" },
+  { column1: 234, date: "2022-11-01T02:49:57.038Z" },
+];
+
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
 const root = ReactDOM.createRoot(document.body)
-root.render(<MyComponent />)
+root.render(<Spreadsheet />)
